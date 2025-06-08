b9adcdada1b27d23bb1d5a2b72b52c98fe269a53
Maksim Pischulenok <pischule@outlook.com>
2025-06-08 23:00:29 +0300

    Add spring boot web starter

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.3.0")
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
     implementation(awssdk.services.s3)
+    implementation("org.springframework.boot:spring-boot-starter-web")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
