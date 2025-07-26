94a59972ec6c32db3623e138ca6e0380505a9681
Maksim Pischulenok <pischule@outlook.com>
2025-07-27 01:48:32 +0300

    Fix error message

--- src/main/kotlin/com/pischule/memevizor/video/VideoTranscodingException.kt
+++ src/main/kotlin/com/pischule/memevizor/video/VideoTranscodingException.kt
@@ -1,4 +1,4 @@
 package com.pischule.memevizor.video
 
 data class VideoTranscodingException(val ffmpegExitCode: Int, val ffmpegOutput: String) :
-    RuntimeException("Video transcoding failed. Code=$ffmpegOutput. Output=$ffmpegOutput")
+    RuntimeException("Video transcoding failed. Code=$ffmpegExitCode. Output=$ffmpegOutput")
